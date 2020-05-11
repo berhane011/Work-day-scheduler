4822a0ade26b21b42f9de6962fd5251a8b5405ea
berhane <berhane4321@gmail.com>
2020-05-10 23:17:59 -0700

    adds daily time work planner

---
+++ script.js
@@ -0,0 +1,38 @@
+var currentDay = moment().format("dddd, MMMM Do YYYY H:mm:ss a");
+$("#currentDay").html(currentDay);
+
+var timeBlock = $(".time-block");
+
+timeBlock.each(function () {
+    var currentTime = moment().format("H");
+    console.log(currentTime);
+    var blockTime = $(this).data('time');
+    console.log(blockTime);
+    var presentColor = $(this).siblings("textarea");
+   
+
+    if (blockTime < currentTime) {
+        presentColor.css('background-color', 'lightgrey');
+    }
+    else if (blockTime > currentTime) {
+        presentColor.css('background-color', 'lightgreen');
+    }
+    else {
+        presentColor.css('background-color', 'lightred');
+    }
+})
+
+$(document).ready(function () {
+    for (i = 0; i < 12; i++) {
+        var hour = i.toString();
+        $("#" + hour).text(JSON.parse(localStorage.getItem(hour)));
+    }
+})
+
+$(".save-btn").on("click", function () {
+
+    var currentValue = $(this).siblings("textarea").val()
+    var id = $(this).siblings("textarea").attr("id")
+    localStorage.setItem(id, JSON.stringify(currentValue))
+
+})
